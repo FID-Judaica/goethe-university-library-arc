77efbd8ed179fd2476e555c1361acbce1c7c6d81
Aaron Christianson <ninjaaron@gmail.com>
2017-05-03 18:52:15 +0200

    getting decent hebrew output

--- arc/decode.py
+++ arc/decode.py
@@ -18,9 +18,32 @@
 # this file under either the MPL or the EUPL.
 import re
 import unicodedata
-from functools import wraps
+import functools
 import deromanize
-from deromanize import trees
+from deromanize import trees, keygenerator
+from HspellPy import Hspell
+
+
+class reify:
+    """ Use as a class method decorator.  It operates almost exactly like the
+    Python ``@property`` decorator, but it puts the result of the method it
+    decorates into the instance dict after the first call, effectively
+    replacing the function it decorates with an instance variable.  It is, in
+    Python parlance, a non-data descriptor.
+
+    Stolen from pyramid.
+    http://docs.pylonsproject.org/projects/pyramid/en/latest/api/decorator.html#pyramid.decorator.reify
+    """
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        functools.update_wrapper(self, wrapped)
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
 
 
 # deromanizing in involves doing special stuff at the start and end of the
@@ -37,7 +60,7 @@ def junker(word):
     junk = []
     remainder = ''
     for i, char in enumerate(word):
-        if unicodedata.category(char)[0] != 'L':
+        if unicodedata.category(char)[0] == 'L':
             remainder = word[i:]
             break
         junk.append(char)
@@ -55,7 +78,7 @@ def double_junker(word):
 
 def junk_splitter(func):
     '''decorator to ignore punctuation before and after words'''
-    @wraps(func)
+    @functools.wraps(func)
     def split_func(word, *args, **kwargs):
         front_junk, stripped_word, back_junk = double_junker(word)
         if stripped_word:
@@ -89,8 +112,14 @@ class Decoder:
         """Return a list of Word instances from a given line of input."""
         cleanedline = self.cleanline(line)
         chunks = self.makechunks(cleanedline)
+        hebwords = []
+        for chunk in chunks:
+            if isinstance(chunk, list):
+                hebwords.append(deromanize.add_reps([i.heb for i in chunk]))
+            else:
+                hebwords.append(get_self_rep(chunk))
 
-        return chunks
+        return hebwords
 
     def makechunks(self, line):
         rawchuncks = [i.split('-') for i in line.split()]
@@ -98,16 +127,21 @@ class Decoder:
         newchunk = []
         for chunk in rawchuncks:
             newchunk = []
+            if chunk == ['', '']:
+                remixed.append('-')
+                continue
             for i, inner in enumerate(chunk[:-1]):
                 if self.checkprefix(i, inner, chunk):
-                    newchunk.append(inner)
+                    newchunk.append(Prefix(inner, self.keys))
                 else:
-                    remixed.append(([inner, '-']))
+                    newchunk.append(Word(inner, self.keys))
+                    remixed.extend([newchunk, [maqef]])
+                    newchunk = []
             if newchunk:
-                newchunk.append(chunk[-1])
+                newchunk.append(Word(chunk[-1], self.keys))
                 remixed.append(newchunk)
             else:
-                remixed.append([chunk[-1]])
+                remixed.append([Word(chunk[-1], self.keys)])
         return remixed
 
     def checkprefix(self, i, inner, chunk):
@@ -136,17 +170,122 @@ class Decoder:
         if line[0] == '[' and line[-1] == ']' and ']' not in line[:-1]:
             line = line[1:-1]
             rebracket = True
-
         if '[' in line:
             line = re.sub(r'\[.*?\]', '', line)
-
         if rebracket:
             line = '[' + line + ']'
-
         if '- ' in line:
             line = re.sub(r'(\w)- +([^@])', r'\1-\2', line)
-
         if ' -' in line:
             line = re.sub(r' -(\w)', r'-\1', line)
-
+        if line.startswith('ha'):
+            line = re.sub('^ha\w{0,2}- *@', 'h @', line)
         return line
+
+
+class Word:
+    def __init__(self, word, keys):
+        self.word = word
+        self.junked = double_junker(word)
+        self.keys = keys
+
+    @reify
+    def heb(self):
+        front, rom, back = self.junked
+        try:
+            word = coredecode(self.keys, rom)
+        except KeyError:
+            return get_self_rep(self.word)
+        except IndexError:
+            return
+        if front:
+            word = get_self_rep(front) + word
+        if back:
+            word += get_self_rep(back)
+        return word
+
+    def __repr__(self):
+        return f"Word({self.word!r})"
+
+
+class Prefix(Word):
+    @reify
+    def heb(self):
+        front, rom, back = self.junked
+        if rom in u:
+            word = keygenerator.ReplacementList(rom, ['ו'])
+        else:
+            word = self.keys['front'][rom[0]]
+
+        if front:
+            word = get_self_rep(front) + word
+        if back:
+            word += get_self_rep(back)
+        return word
+
+    def __repr__(self):
+        return f"Prefix({self.word!r})"
+
+
+def coredecode(keys, word):
+    if word == '':
+        return get_self_rep(word)
+    replist = _coredecode(keys, word)
+    for i in replist:
+        if hspell.check_word(str(i)):
+            i.weight = -1
+    replist.prune()
+    return replist
+
+
+def _coredecode(keys, word):
+    # add aleph to words that begin with vowels.
+    if word[0] in keys.profile['vowels']:
+        word = 'ʾ' + word
+    # remove doubled letters
+    newword = ''
+    for i, c in enumerate(word):
+        if i == 0 or c != word[i-1]:
+            newword += c
+    word = newword
+    # get ending clusters, then beginning clusters, then whatever's left in the
+    # middle.
+    end, remainder = keys['end'].getpart(word)
+    if remainder:
+        try:
+            front, remainder = keys['front'].getpart(remainder)
+        except KeyError:
+            return no_end(keys, word)
+    else:
+        return no_end(keys, word)
+
+    if remainder:
+        middle = keys['mid'].getallparts(remainder).add()
+        return (front + middle + end)
+    else:
+        return (front + end)
+
+
+def no_end(keys, word):
+    # this is where words go when getting the ending first produces strange
+    # results.
+    front, remainder = keys['front'].getpart(word)
+    if remainder:
+        end, remainder = keys['end'].getpart(remainder)
+        if remainder:
+            middle = keys['mid'].getallparts(remainder).add()
+            return (front + middle + end)
+        else:
+            return (front + end)
+    else:
+        return (front)
+
+
+def get_self_rep(string):
+    return keygenerator.ReplacementList(string, [string])
+
+
+maqef = keygenerator.ReplacementList('-', ['־'])
+maqef.heb = maqef
+u = {'û', 'u'}
+hspell = Hspell()
