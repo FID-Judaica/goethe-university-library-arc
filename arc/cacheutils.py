c2d6db818926b8448dadd9063b1b05056aeb4317
Aaron Christianson <ninjaaron@gmail.com>
2017-08-22 11:29:34 +0200

    derp

---
+++ arc/cacheutils.py
@@ -0,0 +1,38 @@
+from deromanize import cacheutils
+import unicodedata
+VOWELS = set('ieaou')
+gstops = {'ʿ', 'ʾ'}
+
+
+def loc_coverter_factory(simple_reps, set_reps):
+    replace = cacheutils.replacer_maker(simple_reps, set_reps)
+
+    def get_loc(rep):
+        flat_vowels = cacheutils.strip_chars(rep.keyvalue)
+        try:
+            loc = ''.join((i[0] for i in replace(flat_vowels)))
+        except TypeError:
+            print(rep.keyvalue)
+            loc = 'whatever'
+        if len(loc) > 1:
+            if loc[0] == 'ʾ':
+                loc = loc[1:]
+            if loc[-1] == 'ʾ':
+                loc = loc[-1]
+            loc = loc.replace('-ʾ', '-')
+        return loc
+
+    return get_loc
+
+
+def loc2phon(loc):
+    phon = loc.replace('ḥ', 'ch').replace('kh', 'ch')
+    if phon[-1] == 'h' and phon[-2:-1] in VOWELS:
+        phon = phon[:-1]
+    phon = ''.join(
+        c for c in unicodedata.normalize('NFD', phon)
+        if unicodedata.category(c)[0] == 'L' and c not in gstops
+    )
+    for v in VOWELS:
+        phon = phon.replace(v+v, v+"'"+v)
+    return phon
---
+++ arc/util.py
@@ -0,0 +1,70 @@
+import sys
+import argparse
+import yaml
+import arc
+from pathlib import Path
+from . import cacheutils
+PROJ_PATH = Path(arc.__file__).parents[1]
+
+
+def expand_kv(key, value):
+    lk, lv = len(key), len(value)
+    if lk > lv:
+        value = value + ' ' * (lk-lv)
+    else:
+        key = key + ' ' * (lv-lk)
+    return key, value
+
+
+def main():
+    ap = argparse.ArgumentParser(description='show some Hebrew things')
+    add = ap.add_argument
+    add('--reverse-heb', '-r', action='store_true',
+        help='reverse output for terminal')
+    add('--show-new', '-n', action='store_true',
+        help='Show the corrected from of the word with LoC transliteration')
+    add('--debug', '-d', action='store_true', help='show debugging info')
+    add('--numbers', '-N', action='store_true', help='show "secret" numbers')
+    add('--crop', '-c', type=int, default=0)
+    add('--sep', '-s', default='│')
+    add('--standard', default='old')
+    add('--loc', '-l', action='store_true')
+    args = ap.parse_args()
+
+    if args.loc:
+        args.standard = 'loc'
+
+    if args.standard == 'old':
+        config_file = PROJ_PATH/'data'/'old.yml'
+    elif args.standard in ['loc', 'new']:
+        config_file = PROJ_PATH/'data'/'new.yml'
+    decoder = arc.Decoder(
+        yaml.safe_load(config_file.open()), fix_numerals=True)
+    set_reps = decoder.profile['to_new']['sets']
+    simple_reps = decoder.profile['to_new']['replacements']
+    global replace
+    get_loc = cacheutils.loc_coverter_factory(simple_reps, set_reps)
+
+    for t in map(str.rstrip, sys.stdin):
+        print(t)
+        for word in decoder.decode(t):
+            word.prune()
+            for i, w in enumerate(word):
+                if args.crop and args.crop == i:
+                    break
+                items = []
+                items.append(str(w)[::-1] if args.reverse_heb else w)
+                if args.show_new:
+                    items.append(get_loc(w))
+                if args.numbers:
+                    items.append(w.weight)
+                print(*items, sep='\t')
+                if args.debug:
+                    atoms = [expand_kv(k, v) for k, v in w.keyvalue]
+                    print('  ' + args.sep.join(rom for rom, _ in atoms))
+                    print('  ' + args.sep.join(heb for _, heb in atoms) + '\n')
+        print()
+
+
+if __name__ == '__main__':
+    main()
