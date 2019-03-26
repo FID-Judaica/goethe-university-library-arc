ac43abdd819e62c71037751230e68afa24178be9
Aaron Christianson <ninjaaron@gmail.com>
2019-03-26 13:15:48 +0100

    tryinna generalize my functions a little more

--- arc/picaqueries.py
+++ arc/picaqueries.py
@@ -21,6 +21,13 @@ def hastransciption(subs):
     return False
 
 
+def islatinfield(subs):
+    lang = subs.get("U")
+    if lang is None or lang == "Latn":
+        return True
+    return False
+
+
 def getnameparts(subs):
     nameparts = []
     last = subs.get("a")
@@ -39,16 +46,24 @@ def getnameparts(subs):
 
 
 def getnames(record):
+    for field in namefields:
+        field = record.get(field)
+        if field:
+            yield from map(getnameparts, field)
+
+
+def getsortednames(record):
     names = []
     nottrans = []
     for field in namefields:
         field = record.get(field)
         if field:
             for subs in field:
-                if hastransciption(subs):
-                    names.append(subs)
-                elif subs.get("U", "") != "Hebr":
-                    nottrans.append(subs)
+                if islatinfield(subs):
+                    if hastransciption(subs):
+                        names.append(subs)
+                    else:
+                        nottrans.append(subs)
     transnames = [getnameparts(n) for n in names]
     transnames = list(filter(None, transnames))
     othernames = [getnameparts(n) for n in nottrans]
@@ -62,13 +77,17 @@ class Title(t.NamedTuple):
     responsibility: t.Union[str, None]
 
 
+def gettitle(subs):
+    return Title(*map(field.getone, "adh"))
+
+
+def fields2transtitle(fields):
+    for field in fields:
+        if islatinfield(field):
+            break
+    return gettitle(fields)
+
+
 def gettranstitle(record, fieldname="021A"):
     fields = record[fieldname]
-    for field in fields:
-        lang = field.get("U")
-        if lang is None or lang == "Latn":
-            maintitle = field.getone("a")
-            break
-    subtitle = field.getone("d")
-    resp = field.getone("h")
-    return Title(maintitle, subtitle, resp)
+    return fields2title(fields)
