355613486f2f73765bbb21a6a40476578a4d63ef
Aaron Christianson <ninjaaron@gmail.com>
2019-05-07 15:43:37 +0200

    multiple small changes to config. lots of doc strings. some other stuff.

--- arc/config.py
+++ arc/config.py
@@ -1,29 +1,51 @@
+"""config objects and session objects to build decoders, caches and all
+kinds of other useful things for retro-conversion.
+"""
 from pathlib import Path
+import libaaron
+import deromanize
+import pica_parse
+from .db import ArcDB
 from . import Decoder
 from . import cacheutils as cu
 from . import filters
-from arc.db import ArcDB
-import deromanize
-import pica_parse
-import libaaron
 
 CACHE_NAMES = "DIN1982", "LOC/ALA", "phonological"
 
 
 class Config(deromanize.Config):
+    """type represeting a config file with methods for generating
+    appropriate data structures from it.
+    """
+
     def __init__(self, path=None, loader=None):
+        """
+        - path: path to configuration file
+        - loader: function for deserializing the config file. YAML is
+          default.
+        """
         super().__init__(path, loader)
         self.db_path = Path(self.user_conf.get("pica_db")).expanduser()
         self.pica_path = Path(self.user_conf.get("pica_file")).expanduser()
 
     def from_schema(self, schema_name, *args, **kwargs):
+        """build a decoder from a schema_name. *args and **kwargs are
+        passed on to arc.decode.Decoder.
+        """
         profile = self.loader(self.schemas[schema_name])
         return Decoder(profile, *args, fix_k=profile.get("fix_k"), **kwargs)
 
     def get_db(self):
+        """initialize the ARC database, which contains pica records as
+        well as some tables for auditing generated results.
+        """
         return ArcDB("sqlite:///" + str(self.db_path))
 
     def get_index(self):
+        """returns a pica index, in the event you don't want to bother
+        with a database. This is actually much faster than using the
+        database, but you can only pull records by ppn.
+        """
         return pica_parse.PicaIndex.from_file(self.pica_path)
 
 
@@ -41,7 +63,7 @@ class Session:
 
     def __init__(self, config: Config):
         """
-
+        config -- a Config instance to pull data from
         """
         self.config = config
         self.records = config.get_db()
@@ -53,19 +75,22 @@ class Session:
 
     @classmethod
     def fromconfig(cls, path=None, loader=None):
+        """takes the same arguments as the ``Config`` initializer,
+        constructs the config object and uses it to build a session.
+        """
         return cls._sessions.setdefault(
             (path, loader), cls(Config(path=path, loader=loader))
         )
 
     def add_decoder(self, name, *args, **kwargs):
-        d = self.decoders.setdefault(
+        decoder = self.decoders.setdefault(
             name, self.config.from_schema(name, *args, **kwargs)
         )
         if name == "old" and not hasattr(self, "getloc"):
-            set_reps = d.profile["to_new"]["sets"]
-            simple_reps = d.profile["to_new"]["replacements"]
+            set_reps = decoder.profile["to_new"]["sets"]
+            simple_reps = decoder.profile["to_new"]["replacements"]
             self.getloc = cu.loc_converter_factory(simple_reps, set_reps)
-        return d
+        return decoder
 
     def add_decoders(self, names, *args, **kwargs):
         return [self.add_decoder(name, *args, **kwargs) for name in names]
@@ -74,8 +99,7 @@ class Session:
         line = filters.Line(string)
         if line.has("only_new") or line.has("new_digraphs"):
             return self.decoders.new
-        else:
-            return self.decoders.old
+        return self.decoders.old
 
     def getchunks(self, string: str):
         decoder = self.pickdecoder(string)
