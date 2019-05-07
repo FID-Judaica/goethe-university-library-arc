0cb053294eac5379553dde49e758ec4bdfcd637e
Aaron Christianson <ninjaaron@gmail.com>
2019-05-07 15:44:08 +0200

    ran black

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ import fastentrypoints
 from setuptools import setup
 
 setup(
-    name='arc',
-    version='0.0',
-    author='FID-Judaica, Goethe Universitätsbibliothek',
-    license='MLP 2.0/EUPL 1.1',
-    author_email='a.christianson@ub.uni-frankfurt.de',
+    name="arc",
+    version="0.0",
+    author="FID-Judaica, Goethe Universitätsbibliothek",
+    license="MLP 2.0/EUPL 1.1",
+    author_email="a.christianson@ub.uni-frankfurt.de",
     # url='https://github.com/FID-Judaica/-parse.py',
-    description='transliteration to Hebrew translator',
-    long_description=open('README.rst').read(),
-    packages=['arc'],
-    entry_points={'console_scripts': [
-        'fl=arc.filters:main',
-        'derom=arc.util:main']},
+    description="transliteration to Hebrew translator",
+    long_description=open("README.rst").read(),
+    packages=["arc"],
+    entry_points={
+        "console_scripts": ["fl=arc.filters:main", "derom=arc.util:main"]
+    },
     install_requires=[
-        'pica_parse',
-        'filtermaker',
-        'deromanize',
-        'PyYaml',
-        'sqlalchemy',
-        'libaaron',
+        "pica_parse",
+        "filtermaker",
+        "deromanize",
+        "PyYaml",
+        "sqlalchemy",
+        "libaaron",
     ],
 )
--- show_combos.py
+++ show_combos.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import deromanize
 import yaml
 from deromanize import cacheutils
-CONFIG_FILE = Path(__file__).parent/'data'/'old.yml'
+
+CONFIG_FILE = Path(__file__).parent / "data" / "old.yml"
 keys = deromanize.KeyGenerator(yaml.safe_load(CONFIG_FILE.open()))
 
 
@@ -11,10 +12,11 @@ def main():
     pairs = set()
     for key in keys:
         pairs.update(
-                set(cacheutils.strip_chars(cacheutils.get_combos(keys[key]))))
-    print(*sorted(pairs, key=lambda x: (x[1], x[0])), sep='\n')
+            set(cacheutils.strip_chars(cacheutils.get_combos(keys[key])))
+        )
+    print(*sorted(pairs, key=lambda x: (x[1], x[0])), sep="\n")
     print(len(pairs))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
